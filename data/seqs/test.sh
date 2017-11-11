b70d3708ed388e919b6e155a73799df2c3b055f1
MathiasRenaud <mathias.renaud5@gmail.com>
2017-11-11 15:36:44 -0500

    bash scripts and sequence folders for farming out annotation work

--- data/seqs/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e  # abort script upon nonzero exit status
-set -u  # abort script if variable's value is unset
-set -o pipefail # abort script if any error in pipe
-
-for file in ../data/seqs/*.fa
-do tag=${file%.fa}
-echo "tag: $tag"
-done
---
+++ scripts/nicole.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # abort script upon nonzero exit status
+set -u  # abort script if variable's value is unset
+#set -o pipefail # abort script if any error in pipe (illegal in BioLinux??)
+
+# run annotation.sh from /scripts directory on FASTA files in /seqs
+cd ../data/seqs/nicole
+
+for file in *.fa
+do tag=${file%.fa}
+# echo "tag: $tag"  # for testing purposes
+prokka --prefix "$tag" --locustag "$tag" --cpus 2 --centre C --locustag L --compliant --mincontiglen 200 --outdir ../../../analysis/prokka/"$tag" --force --addgenes "$file"
+done
