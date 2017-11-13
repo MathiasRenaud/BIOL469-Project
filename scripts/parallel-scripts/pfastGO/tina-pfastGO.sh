376eae44170c2f6c0aff4f4ad7ad8a99f6f7ac67
MathiasRenaud <mathias.renaud5@gmail.com>
2017-11-13 17:00:58 -0500

    scripts for running on 4 machines

---
+++ scripts/parallel-scripts/pfastGO/tina-pfastGO.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e  # abort script upon nonzero exit status
+set -u  # abort script if variable's value is unset
+#set -o pipefail # abort script if any error in pipe (illegal in BioLinux??)
+
+cd ../analysis/proteins/tina
+
+for file in *.faa
+do tag=${file%.*.*.*.faa}
+
+sh pfastGO.sh "$file" "$tag" 2
+
+# for testing purposes
+#echo "tag: $tag"
+#echo "file: $file"
+done
