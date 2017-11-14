dd9fc0506dac426216edffa2fd2b3ba6707e0394
MathiasRenaud <mathias.renaud5@gmail.com>
2017-11-14 12:08:07 +0000

    fixes compress script

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -3,13 +3,13 @@ set -e  # abort script upon nonzero exit status
 set -u  # abort script if variable's value is unset
 #set -o pipefail # abort script if any error in pipe (illegal in BioLinux??)
 
-cd ../analysis/proteins/*/temp
+cd ../analysis/proteins/*/results
 
 for dir in */ ; do
 
 tag=${dir%/}
 
-tar -czvf "$tag".tar.gz /home/manager/BIOL469-Project/analysis/proteins/*/temp/"$dir"
+tar -czvf "$tag".tar.gz /home/manager/BIOL469-Project/analysis/proteins/*/results/"$dir"
 
 
 # for testing purposes
